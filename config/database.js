c44d79853203b311d11830efc4b42f79a454698e
AerialWombat <adrianzhan@gmail.com>
2019-04-09 17:20:37 -0400

    configured knes for pg database

---
+++ config/database.js
@@ -0,0 +1,11 @@
+const knex = require("knex");
+
+const database = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    database: "open-times"
+  }
+});
+
+module.exports = database;
