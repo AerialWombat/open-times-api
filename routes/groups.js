a20a00a449d86f400ce38a30c9be895582ab4960
AerialWombat <adrianzhan@gmail.com>
2019-05-20 18:09:14 -0400

    reorganize. add schedule set functionality

--- routes/groups.js
+++ routes/groups.js
@@ -39,10 +39,10 @@ router.post('/create', (req, res) => {
   // Transaction to append new group to user record and then insert new group
   database.transaction(trx => {
     return trx('users')
-      .where('username', '=', username)
       .update('groups', database.raw('array_append(groups, ?)', [uuid]), [
         'groups'
       ])
+      .where({ username: username })
       .then(() => {
         return trx('groups')
           .returning('slug')
@@ -64,7 +64,6 @@ router.post('/create', (req, res) => {
           });
       })
       .then(slug => {
-        // trx.commit;
         res.status(200).json({ slug: slug[0] });
       })
       .catch(trx.rollback);
@@ -73,49 +72,99 @@ router.post('/create', (req, res) => {
 
 router.post('/set-schedule', (req, res) => {
   const { UUID, schedule } = req.body;
-  const { username } = req.user;
+  let username;
 
-  database('schedules')
-    .where({
-      username: username,
-      group_id: UUID
-    })
-    .select()
-    .then(scheduleRecords => {
-      if (scheduleRecords.length <= 0) {
-        database('schedules')
-          .insert({
-            username: username,
-            group_id: UUID,
-            schedule: schedule
-          })
-          .then(() => {
-            res.status(200).send('Successfully set schedule!');
-          })
-          .catch(error => {
-            console.log('Error inserting into SCHEDULES.', error);
-            res.status(500).send('Error setting schedule.');
+  // Check if logged in. Then add member to group. If logged in, add group to member's groups list.
+  if (req.user) {
+    username = req.user.username;
+
+    database('groups')
+      .select('members')
+      .where({ slug: UUID })
+      .then(memberRecord => {
+        const members = memberRecord[0].members;
+        if (members.find(member => member === username)) {
+          // Member is already in group
+        } else {
+          // Member is not in group
+          database.transaction(trx => {
+            return trx('groups')
+              .update(
+                'members',
+                database.raw('array_append(members, ?)', [username]),
+                ['members']
+              )
+              .where('slug', '=', UUID)
+              .then(() => {
+                return trx('users')
+                  .update(
+                    'groups',
+                    database.raw('array_append(groups, ?)', [UUID]),
+                    ['groups']
+                  )
+                  .where('username', '=', username);
+              })
+              .catch(trx.rollback);
           });
-      } else {
-        database('schedules')
-          .where({
-            username: username,
-            group_id: UUID
-          })
-          .update({
-            username: username,
-            group_id: UUID,
-            schedule: schedule
-          })
-          .then(() => {
-            res.status(200).send('Successfully updated schedule!');
-          })
-          .catch(error => {
-            console.log('Error updating SCHEDULES.', error);
-            res.status(500).send('Error updating schedule.');
-          });
-      }
-    });
+        }
+      });
+  } else if (req.body.anonUsername) {
+    username = req.body.anonUsername;
+
+    database('groups')
+      .update('members', database.raw('array_append(members, ?)', [username]), [
+        'members'
+      ])
+      .where({ slug: UUID })
+      .catch(error => console.log(error));
+  }
+
+  // Add or update schedule in schedules table
+  if (username) {
+    database('schedules')
+      .select()
+      .where({
+        username: username,
+        group_id: UUID
+      })
+      .then(scheduleRecords => {
+        if (scheduleRecords.length <= 0) {
+          database('schedules')
+            .insert({
+              username: username,
+              group_id: UUID,
+              schedule: schedule
+            })
+            .then(() => {
+              res.status(200).send('Successfully set schedule!');
+            })
+            .catch(error => {
+              console.log('Error inserting into SCHEDULES.', error);
+              res.status(500).send('Error setting schedule.');
+            });
+        } else {
+          database('schedules')
+            .update({
+              username: username,
+              group_id: UUID,
+              schedule: schedule
+            })
+            .where({
+              username: username,
+              group_id: UUID
+            })
+            .then(() => {
+              res.status(200).send('Successfully updated schedule!');
+            })
+            .catch(error => {
+              console.log('Error updating SCHEDULES.', error);
+              res.status(500).send('Error updating schedule.');
+            });
+        }
+      });
+  } else {
+    res.status(400).send('Username is missing.');
+  }
 });
 
 router.get('/info/:groupID', (req, res) => {
@@ -123,9 +172,7 @@ router.get('/info/:groupID', (req, res) => {
 
   database('groups')
     .select('name', 'location', 'description', 'members')
-    .where({
-      slug: groupID
-    })
+    .where({ slug: groupID })
     .then(groupInfo => {
       const { name: title, location, description, members } = groupInfo[0];
       res.status(200).json({
@@ -143,9 +190,7 @@ router.get('/view/:groupID', (req, res) => {
 
   database('schedules')
     .select('username', 'schedule')
-    .where({
-      group_id: groupID
-    })
+    .where({ group_id: groupID })
     .then(scheduleRecords => {
       // Iterate through each hour block
       for (let i = 0; i < 168; i++) {
@@ -172,9 +217,7 @@ router.get('/view/:groupID', (req, res) => {
     .then(() => {
       database('groups')
         .select('name', 'location', 'description', 'members')
-        .where({
-          slug: groupID
-        })
+        .where({ slug: groupID })
         .then(groupInfo => {
           const { name: title, location, description, members } = groupInfo[0];
           res.status(200).json({
